c192497856999f7c01e12bcfc9018ac0c74dda66
CoolBro4811 <121374566+CoolBro4811@users.noreply.github.com>
2024-05-18 15:11:44 -0700

    Update readFile.py
    fixed some bad code

--- readFile.py
+++ readFile.py
@@ -4,18 +4,16 @@ class ReadFile():
   def __init__(self, filename:str) -> None:
     self.filename = filename
     with open(filename, "r") as file:
-      self.file = file
-      lines = self.file.readlines()
-    
-    self.lines = lines
+      lines = file.readlines()
+      self.lines = lines
+      file.close()
+      
 
 class WriteFile():
   def __init__(self, filename:str, lines:list[str]) -> None:
     self.filename = filename
+    self.lines = lines
     with open(filename, "r") as file:
-      self.file = file
-      self.write(lines)
-
-  def write(self, lines:list[str]) -> None:
-    for line in lines:
-      self.file.write(line)
+      for line in self.lines:
+        file.write(line)
+      file.close()
